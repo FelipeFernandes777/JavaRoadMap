cddd12ece5c586ebb3f86bae790cea955824885e
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2024-05-17 21:43:19 -0300

    Diretorios temporarios

---
+++ WorkingFilesAndApis/CreatedTemporaryDirectory.java
@@ -0,0 +1,13 @@
+package WorkingFilesAndApis;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class CreatedTemporaryDirectory {
+
+    /**Além dos arquivos, você também pode criar diretórios temporários. Como você não precisa do parâmetro suffix ao criar diretórios, você só precisa escolher especificar um parâmetro prefixo. */
+    public static void main(String[] args) throws IOException {
+        Path tmpDirectory = Files.createTempDirectory("Novo Diretorio");
+    }
+}
--- WorkingFilesAndApis/WorkingFilesAndAPIs.java
+++ WorkingFilesAndApis/WorkingFilesAndAPIs.java
@@ -1,7 +1,4 @@
 package WorkingFilesAndApis;
-
-import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
