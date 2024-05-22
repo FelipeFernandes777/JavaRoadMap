b9075fa579baffab1b7f936e91835c943dcb5eee
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2024-05-22 19:49:43 -0300

    Criação de diretorios e arquivos

--- WorkingFilesAndApis/CreatedDirectory.java
+++ WorkingFilesAndApis/CreatedDirectory.java
@@ -6,10 +6,10 @@ import java.nio.file.Path;
 
 public class CreatedDirectory {
 
-    private static WorkingFilesAndAPIs path = new WorkingFilesAndAPIs();
+    private static Path path = Path.of("C:\\Users\\Felipe Fernandes\\Documents\\My Games");
 
     public static void main(String[] args) throws Exception {
-        Path newDirectory = Files.createDirectories(path.getPath().getParent().resolve("/new/source/dir"));
+        Path newDirectory = Files.createDirectories(path.getParent().resolve("/new/source/dir"));
         System.out.println(newDirectory);
 
         /** .resolve() não cria o arquivo, apenas retorna uma referência ao arquivo (filho) que você está prestes a criar. */
