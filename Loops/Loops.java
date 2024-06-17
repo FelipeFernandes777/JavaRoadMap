d7bd0f29fc00331ce21d4cd749cbb6760fcc4faa
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2024-06-17 20:20:30 -0300

    Começo dos estudos sobre loop

--- Loops/Loops.java
+++ Loops/Loops.java
@@ -4,11 +4,15 @@ public class Loops {
     public static void main(String[] args) {
         // Mostre os numeros de 1 a 5
 
+        sintaxFor();
+    }
+
+    public static  void sintaxFor(){
         /**
-            Syntax for loops:
+         Syntax for loops:
 
          for (initialisation; condition; increment/decrement) {
-            //body
+         //body
          }
          */
 
