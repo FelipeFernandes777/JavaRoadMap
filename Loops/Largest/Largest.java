f1710357a7493362cf521b610600ea950151ce08
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2024-06-18 16:15:12 -0300

    Estudos sobre for while e do while

---
+++ Loops/Largest/Largest.java
@@ -0,0 +1,4 @@
+package Loops.Largest;
+
+public class Largest {
+}
--- Loops/Loops.java
+++ Loops/Loops.java
@@ -1,23 +1,67 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args) {
         // Mostre os numeros de 1 a 5
 
-        sintaxFor();
+        sintaxDoWhile();
     }
 
-    public static  void sintaxFor(){
+    public static void sintaxFor(){
         /**
          Syntax for loops:
 
-         for (initialisation; condition; increment/decrement) {
-         //body
+         for (Inicialização; condição; Incremento/Decremento) {
+         //Instrução ou codigo a ser executado
          }
          */
 
+        //Recomendado usar quando se sabe a quantidade de vezes que o loop será executado
         for (int num = 1; num <= 5; num++){
             System.out.println(num);
         }
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int num = 1; num <= n; num++){
+            System.out.println(num + " ");
+        }
+    }
+
+    public static void sintaxWhile(){
+        /**
+          Syntax while loops:
+
+            while(condição) {
+                //Instrução ou codigo que será executado
+            }
+         **/
+
+
+        int num = 1;
+        //Recomendado usar quando não se sabe a quantidade total de vezes que o loop será executado.
+        while (num <= 5) {
+            System.out.println(num);
+            num++;
+        }
+    }
+
+    public static void sintaxDoWhile(){
+        /**
+          Syntax while loops:
+            do{
+         //Instrução ou codigo que será executado
+         }
+         while(condição) {
+            }
+         **/
+
+        int num = 1;
+        do {
+            System.out.println("Hello World");
+        } while (num != 1);
     }
 }
